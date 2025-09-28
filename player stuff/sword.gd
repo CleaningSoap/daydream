66b5f90e6ba67ace235e88cf81f8627829edbc19
CleaningSoap <bruce.otw@gmail.com>
2025-09-27 20:08:37 -0400

    hp bar
    added health bar basically

--- player stuff/sword.gd
+++ player stuff/sword.gd
@@ -1,6 +1,17 @@
 extends Area2D
 @onready var pivot: Marker2D = $Pivot
 @onready var attack_range: CollisionShape2D = $"Attack Range"
+@onready var enemy: Node2D = $"."
 
-func _physics_process(delta):
-	var enemies_in_range = get_overlapping_bodies()
+
+
+#func _physics_process(delta: float):
+	
+#	if Input_is_just_pressed("attack"):
+	
+
+
+
+
+func _on_area_entered(area: Area2D) -> void:
+	pass # Replace with function body.
