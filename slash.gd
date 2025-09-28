517b2bd94f2c0ff4d29602522b2d5611b8f2ef36
CleaningSoap <bruce.otw@gmail.com>
2025-09-28 02:47:22 -0400

    update slash
    slash spawn point etc

--- slash.gd
+++ slash.gd
@@ -1,6 +1,17 @@
 extends Area2D
+var distance_travelled = 0
 
 func _physics_process(delta: float) -> void:
-	var direction = Vector2.RIGHT
+	const SPEED = 1000
+	const RANGE = 1200
+	var direction = Vector2.RIGHT.rotated(rotation)
+	position += direction * SPEED * delta
 	
-	
+	distance_travelled += SPEED * delta
+	if distance_travelled > RANGE:
+		queue_free()
+
+
+
+func _on_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
