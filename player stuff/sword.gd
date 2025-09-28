8d740197892e5cbac4736b37402700aa4b82fed0
CleaningSoap <bruce.otw@gmail.com>
2025-09-28 04:41:19 -0400

    hp sacrifices work now
    can buff sword

--- player stuff/sword.gd
+++ player stuff/sword.gd
@@ -6,13 +6,15 @@ extends Area2D
 @onready var slash_cd: Timer = $"Slash CD"
 var player
 const PLAYERPATH = "/root/Main/Player"
+@onready var attack_range: CollisionShape2D = $"Attack Range"
 
 @onready var enemy: Node2D = $"."  
 var can_attack = true
-var can_slash = true
+var can_slash = false
 signal attack_damage(a_damage : int)
 var damage = 25
 var last_dir = 0  # Initialize last_dir here
+var unlocked_slash = false
 
 func _ready():
 	player = get_node(PLAYERPATH)
@@ -22,7 +24,7 @@ func _physics_process(_delta):
 			
 	if dir != 0:  # Only update last_dir when the player moves
 		last_dir = dir  
-	if can_slash:
+	if can_slash && unlocked_slash:
 		if Input.is_action_just_pressed("slash_attack"):
 			if last_dir == -1:  # Facing left
 				rotation += 130
@@ -56,8 +58,9 @@ func _on_attack_speed_timeout() -> void:
 	
 
 func _on_slash_cd_timeout():
-	rotation = 0
-	can_slash = true
+	if unlocked_slash:
+		rotation = 0
+		can_slash = true
 
 func _slash_attack():
 	const SLASH = preload("res://slash.tscn")
@@ -65,9 +68,13 @@ func _slash_attack():
 	new_slash.global_position = %Slash_Point.global_position
 	%Slash_Point.add_child(new_slash)
 
-func _on_button_2_pressed() -> void:
-	damage += 15
 
 func _on_button_pressed() -> void:
-	self.scale = Vector2(2, 2)
-	get_tree().pause = false
+	scale *= 8
+	attack_range.scale *= 8
+
+func _on_button_2_pressed() -> void:
+	damage += 20
+
+func _on_button_3_pressed() -> void:
+	unlocked_slash = true
